dd5b60480d1902a8c6f86810e9236f221f6ad923
atherapath <stewardconductor@outlook.com>
2025-11-15 01:46:47 +0000

    Update pageweaver.js

--- pageweaver.js
+++ pageweaver.js
@@ -63,11 +63,6 @@
         // already-converted headings
         if (/^<h\d>/.test(trimmed)) return trimmed;
 
-        // horizontal rules: --- *** ___
-        if (/^(-{3,}|_{3,}|\*{3,})$/.test(trimmed)) {
-          return "<hr>";
-        }
-
         // blockquotes: lines starting with ">"
         if (/^>\s?/.test(trimmed)) {
           const inner = trimmed.replace(/^>\s?/gm, "");
